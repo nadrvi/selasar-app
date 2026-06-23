4c2d84604fa7aa47a0f9bc909a3b51ec66fbacd4
Raffy Allen <raffy.gilardino29@gmail.com>
2026-06-23 08:47:11 +0700

    Membuat halaman Map dasar serta menambahkan Header dan Logo

---
+++ src/pages/Map.jsx
@@ -0,0 +1,27 @@
+import {
+  FaSearch,
+  FaBook,
+  FaCoffee,
+  FaUsers,
+  FaUniversity,
+  FaTree,
+  FaPlug,
+  FaWifi,
+  FaRegBookmark,
+  FaMapMarkerAlt,
+  FaCrosshairs,
+  FaLocationArrow,
+} from "react-icons/fa";
+
+export default function Map() {
+  return (
+    <div className="min-h-screen bg-[#F4EFE8]">
+
+      {/* HEADER */}
+      <div className="bg-[#F4EFE8] px-4 pt-4">
+
+        {/* Logo */}
+        <div className="flex flex-col items-center">
+          <h1 className="text-[52px] font-serif font-bold text-[#445234] leading-none">
+            Selasar
+          </h1>
